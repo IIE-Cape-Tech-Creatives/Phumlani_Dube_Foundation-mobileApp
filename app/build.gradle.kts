6e3b9742c3684c20d6249e0b905182bfa80d35e9
GitOutOfHereNow <ad.silver7@gmail.com>
2024-10-31 23:04:05 +0200

    Added Donation Form.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,12 @@ dependencies {
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
+	implementation(libs.androidx.core.ktx)
+	implementation(libs.androidx.appcompat)
+	implementation(libs.material)
+	implementation(libs.androidx.activity)
+	implementation(libs.androidx.constraintlayout)
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(libs.androidx.espresso.core)
 }
